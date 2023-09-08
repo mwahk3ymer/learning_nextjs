c982bbf23084f529530256179e7fe3fd718ef369
mwahk3ymer <audreymwakima99@gmail.com>
2023-09-08 17:00:50 +0300

    trying to learn

---
+++ app/Posts/new/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      NEW POST
+    </div>
+  )
+}
+
+export default page
---
+++ app/user/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//local host:3000/user
+const page = () => {
+  return (
+    <div>
+      Hello User
+    </div>
+  )
+}
+
+export default page
